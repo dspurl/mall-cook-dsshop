8cadd63d8217131397fe0e9d2d618d5abc4dada4
dspurl <49799121+dspurl@users.noreply.github.com>
2024-07-11 14:09:34 +0800

    更新

--- packages/mall-cook-platform/src/config/global.js
+++ packages/mall-cook-platform/src/config/global.js
@@ -6,12 +6,9 @@
  * @LastEditTime: 2022-11-02 09:10:53
  */
 export default {
-  // baseApi: 'https://www.lanshan-h5.cn:1443/node/',  // 后端接口地址
-  // baseApi: 'http://127.0.0.1:3200/',  // 后端接口地址
-  baseApi: 'http://dsshop.test/api/v1/admin/',  // 后端接口地址
+  baseApi: 'http://tfshop-pro.test/api/v1/admin/',  // 后端接口地址
   // baseApi: 'https://dsshop.dswjcms.com/api/v1/admin/',  // 后端接口地址
-  // viewUrl: 'http://110.42.184.128:9000/#/'          // iframe嵌套的H5地址,本地开发可改为本地启动的H5地址
-  // viewUrl: 'http://localhost:8080/template/#/'          // iframe嵌套的H5地址,本地开发可改为本地启动的H5地址
-  viewUrl: 'http://dsshop.test/template/#/'          // iframe嵌套的H5地址,本地开发可改为本地启动的H5地址
-  // viewUrl: 'https://dsshop.dswjcms.com/template/#/'          // iframe嵌套的H5地址,本地开发可改为本地启动的H5地址
+  // viewUrl: 'http://tfshop-pro.test/template/#/'          // iframe嵌套的H5地址,本地开发可改为本地启动的H5地址
+  // viewUrl: 'https://dsshop.dswjcms.com/template/#/'
+  viewUrl: 'http://localhost:8080/template/#/'
 }
