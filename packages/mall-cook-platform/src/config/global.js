9cb6537f6275057c4c1c6b179865d511031a8425
dspurl <49799121+dspurl@users.noreply.github.com>
2024-08-15 13:00:58 +0800

    更新接口地址

--- packages/mall-cook-platform/src/config/global.js
+++ packages/mall-cook-platform/src/config/global.js
@@ -6,9 +6,9 @@
  * @LastEditTime: 2022-11-02 09:10:53
  */
 export default {
-  baseApi: 'http://tfshop-pro.test/api/v1/admin/',  // 后端接口地址
+  baseApi: 'http://tfshop-pro.dswjcms.test/api/v1/admin/',  // 后端接口地址
   // baseApi: 'https://dsshop.dswjcms.com/api/v1/admin/',  // 后端接口地址
   // viewUrl: 'http://tfshop-pro.test/template/#/'          // iframe嵌套的H5地址,本地开发可改为本地启动的H5地址
   // viewUrl: 'https://dsshop.dswjcms.com/template/#/'
-  viewUrl: 'http://localhost:8080/template/#/'
+  viewUrl: 'http://localhost:8080/h5/#/'
 }
